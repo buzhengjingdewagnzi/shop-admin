596481d8f34d77e4ecc1de07525f862b04e74b04
不正经的王梓涵 <1613792160@qq.com>
2021-07-28 19:26:08 +0800

    状态开关，操作

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -17,14 +17,36 @@
       </el-col>
     </el-row>
     <!-- 表格 -->
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="userList" style="width: 100%">
       <el-table-column type="index" label="#" width="60"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-      <el-table-column prop="emil" label="邮箱"> </el-table-column>
-      <el-table-column prop="address" label="电话"> </el-table-column>
-      <el-table-column prop="address" label="创建时间"> </el-table-column>
-      <el-table-column prop="address" label="用户状态"> </el-table-column>
-      <el-table-column prop="address" label="操作"> </el-table-column>
+      <el-table-column prop="username" label="姓名" width="180">
+      </el-table-column>
+      <el-table-column prop="email" label="邮箱"> </el-table-column>
+      <el-table-column prop="mobile" label="电话"> </el-table-column>
+      <el-table-column label="创建时间">
+        <!-- slot-scope传一个最外层数据源 -->
+        <template slot-scope="scope">
+          {{ scope.row.create_time | fmtdate }}
+        </template>
+      </el-table-column>
+      <el-table-column label="用户状态" width="150">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.mg_state"
+            active-color="#13ce66"
+            inactive-color="rgba(0,0,0,0.2)"
+          >
+          </el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column prop="address" label="操作">
+        <template slot-scope="scope">
+          <!-- size="mini"尺寸  plain透明底色 circle圆形按钮-->
+          <el-button size="mini" type="primary" icon="el-icon-edit" plain circle></el-button>
+          <el-button size="mini" type="success" icon="el-icon-check" plain circle></el-button>
+          <el-button size="mini" type="danger" icon="el-icon-delete" plain circle></el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -35,13 +57,9 @@ export default {
       query: "",
       pagenum: 1,
       pagesize: 5,
-      tableData: [
-        {
-          id: "1",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-        },
-      ],
+      userList: [],
+      //分页
+      total: -1,
     };
   },
   created() {
@@ -57,7 +75,21 @@ export default {
           `users?query=${this.query}&pagenum=${this.pagenum}&pagesize=${this.pagesize}`
         )
         .then((res) => {
-          console.log(res);
+          const {
+            meta: { status, msg },
+            data: { users, total },
+          } = res.data;
+          if (status === 200) {
+            //表格数据赋值
+            this.userList = users;
+            //total赋值
+            this.total = total;
+            //提示
+            this.$message.success(msg);
+          } else {
+            //提示
+            this.$message.warning(msg);
+          }
         });
     },
   },
--- src/main.js
+++ src/main.js
@@ -7,12 +7,17 @@ import MyHttpServer from '@/plugins/http.js'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/reset.css'
 import router from './router/index'
+import moment from 'moment'
 
 // Vue.prototype.$http = axios
 Vue.use(ElementUI)
 Vue.use(MyHttpServer)
 Vue.config.productionTip = false
 
+// 处理日期格式
+Vue.filter('fmtdate',(v)=>{
+  return moment(v).format('YYYY-MM-DD')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
